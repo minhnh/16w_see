5809319b887258a9950933f48d72d01027e6afb1
Minh Nguyen <minhnh.91@gmail.com>
2016-10-17 01:46:09 +0200

    create function to plot PCA results

---
+++ matlab/main.m
@@ -0,0 +1,8 @@
+% load('overleaf/matlab/poseData.mat');
+addpath('overleaf/matlab/');
+
+% pcaCalculation(data_straight, ' Straight');
+% pcaCalculation(data_slight_left, ' Slight Left');
+% pcaCalculation(data_slight_right, ' Slight Right');
+% pcaCalculation(data_left, ' Left');
+pcaCalculation(data_right, ' Right');
--- matlab/pcaCalculation.m
+++ matlab/pcaCalculation.m
@@ -1,9 +1,28 @@
-function [coeffs, projected_coords] = pcaCalculation(data)
-    coeffs = pca(data);
+function pcaCalculation(data, direction)
+    [coeffs, projected_coords, latent] = pca(data);
 
-    data_straight_m = data - repmat(mean(data), [length(data), 1]);
+    figure();
+    subplot(1, 2, 1);
+    plot(projected_coords(:,1), projected_coords(:,2),'+');
+    xlabel('1st Principal Component');
+    ylabel('2nd Principal Component');
+    title(strcat('Projected Coordinates for Going ', direction));
+    subplot(1, 2, 2);
+    biplot(coeffs);
+    xlabel('x');
+    ylabel('y');
+    title('Principal Components in Original Coordinates')
 
-    projected_coords = data_straight_m * coeffs;
+    figure();
+    subplot(2, 1, 1);
+    hist(projected_coords(:,1));
+    xlabel('Projected coordinates');
+    ylabel('Frequency');
+    title(strcat('1st Principal Comp. Histogram for Going ', direction));
+    subplot(2, 1, 2);
+    hist(projected_coords(:,2));
+    xlabel('Projected coordinates');
+    ylabel('Frequency');
+    title(strcat('2nd Principal Comp. Histogram for Going ', direction));
+
 end
-
-
