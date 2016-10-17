0af057ebee0f010f927825892230638dcf884a1f
Minh Nguyen <minhnh.91@gmail.com>
2016-10-17 16:45:12 +0200

    update matlab calibration data

--- matlab/main.m
+++ matlab/main.m
@@ -1,8 +1,12 @@
-% load('overleaf/matlab/poseData.mat');
+load('overleaf/matlab/poseData.mat');
 addpath('overleaf/matlab/');
 
 % pcaCalculation(data_straight, ' Straight');
 % pcaCalculation(data_slight_left, ' Slight Left');
 % pcaCalculation(data_slight_right, ' Slight Right');
 % pcaCalculation(data_left, ' Left');
-pcaCalculation(data_right, ' Right');
+% pcaCalculation(data_right, ' Right');
+
+J2 = undistortImage(imread('/home/minh/frame0000_.jpg'),cameraParams,'OutputView','full');
+figure; imshow(J2);
+title('Full Output View');
