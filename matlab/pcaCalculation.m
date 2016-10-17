f8963d95a23a3711aaf001967a1ea75de21af758
Minh Nguyen <minhnh.91@gmail.com>
2016-10-17 13:42:39 +0200

    update plotting function

--- matlab/pcaCalculation.m
+++ matlab/pcaCalculation.m
@@ -1,19 +1,23 @@
 function pcaCalculation(data, direction)
+
     [coeffs, projected_coords, latent] = pca(data);
 
-    figure();
+    figure(1);
     subplot(1, 2, 1);
     plot(projected_coords(:,1), projected_coords(:,2),'+');
     xlabel('1st Principal Component');
     ylabel('2nd Principal Component');
     title(strcat('Projected Coordinates for Going ', direction));
     subplot(1, 2, 2);
-    biplot(coeffs);
+    coeffs(4)
+    plot(data(:,1),data(:,2),'g+',[mean(data(:,1)),mean(data(:,1))+coeffs(3)],[mean(mean(data(:,2))),mean(mean(data(:,2)))+coeffs(1)],'r',...
+    [mean(data(:,1)),mean(data(:,1))+coeffs(4)],[mean(data(:,2)),mean(data(:,2))+coeffs(2)],'b');
+    grid on
     xlabel('x');
     ylabel('y');
-    title('Principal Components in Original Coordinates')
+    title('PCA vectors')
 
-    figure();
+    figure(2);
     subplot(2, 1, 1);
     hist(projected_coords(:,1));
     xlabel('Projected coordinates');
