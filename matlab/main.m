c5f199631ca462be061158c3de29c7c51e7c0d8a
Minh Nguyen <minhnh.91@gmail.com>
2016-10-24 02:26:50 +0200

    add angular data and their calculation

--- matlab/main.m
+++ matlab/main.m
@@ -3,14 +3,19 @@ addpath('overleaf/matlab/');
 
 % projected_coords_straight =  pcaCalculation(data_straight, ' Straight');
 fitGaussian(data_straight, ' Straight');
+fitGaussian(data_straight_angles, ' Straight');
 % projected_coords_slightLeft =  pcaCalculation(data_slight_left, ' Slight Left');
 fitGaussian(data_slight_left, ' Slight Left');
+fitGaussian(data_slight_left_angles, ' Slight Left');
 % projected_coords_slightRight =  pcaCalculation(data_slight_right, ' Slight Right');
 fitGaussian(data_slight_right, ' Slight Right');
+fitGaussian(data_slight_right_angles, ' Slight Right');
 % projected_coords_left =  pcaCalculation(data_left, ' Left');
 fitGaussian(data_left, ' Left');
+fitGaussian(data_left_angles, ' Left');
 % projected_coords_right = pcaCalculation(data_right, ' Right');
 fitGaussian(data_right, ' Right');
+fitGaussian(data_right_angles, ' Right');
 
 % J2 = undistortImage(imread('/home/minh/frame0000_.jpg'),cameraParams,'OutputView','full');
 % figure; imshow(J2);
