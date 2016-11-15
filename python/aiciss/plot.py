ea9643ac5eb507a1e6c9ad815737bfb24c615108
Minh Nguyen <minhnh.91@gmail.com>
2016-11-15 02:24:14 +0100

    update raw data plots with grids

--- python/aiciss/plot.py
+++ python/aiciss/plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    plt.figure()
     for i in range(1,len(sys.argv)):
         f = sys.argv[i]
         data = np.genfromtxt(f, usecols=(2,3,4))
@@ -18,10 +19,12 @@ def main():
         x_dir = np.cos(data_short[:,2])# + data[:,0]
         y_dir = np.sin(data_short[:,2])# + data[:,1]
         plt.quiver(data_short[:,0], data_short[:,1], x_dir, y_dir, color='red')
+        pass
     plt.ylabel("Y coordinate (mm)")
     plt.xlabel("X coordinate (mm)")
-    plt.title("Recorded trajectories from motion model (Straight movement)")
+    plt.title("Recorded trajectories from motion model (Right movement)")
     plt.axis('equal')
+    plt.grid()
     plt.show()
     pass
 
