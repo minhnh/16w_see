465fffd4e4e10a4eb0b7c4447d0ca73baab6b5c3
Minh Nguyen <minhnh.91@gmail.com>
2016-11-21 14:01:51 +0100

    add comments and better var names

--- python/aiciss/convert_to_csv.py
+++ python/aiciss/convert_to_csv.py
@@ -5,6 +5,17 @@ import os
 
 
 def get_next_index(data, duration, start_index, time_step, index_estimate):
+    """
+    Recursively find index containing time value of a certain duration from
+    the time value at start_index. Good when certain time stes are missing.
+
+    :param data: array where time values are in column 0
+    :param duration: duration from time start_index to be found (seconds)
+    :param start_index: index of reference point
+    :param time_step: time difference between each indices (seconds)
+    :param index_estimate: estimate calculated using time_step
+    :return: index of time value a certain duration from refernce point
+    """
     start_time = data[start_index, 0]
     if data[-1, 0] < start_time + duration:
         return None
@@ -13,18 +24,21 @@ def get_next_index(data, duration, start_index, time_step, index_estimate):
         index_estimate = len(data) - 1
         pass
 
-    time_change = data[index_estimate, 0] - start_time
-    index_change = int((time_change - duration)/time_step + 0.5)
+    actual_time_diff = data[index_estimate, 0] - start_time
+    index_change = int((actual_time_diff - duration)/time_step + 0.5)
 
     if np.abs(index_change) < 1:
-        if time_change > duration:
+        # Breaking condition. Find index with time value > start_time + duration
+        if actual_time_diff > duration:
             return index_estimate
         else:
             while data[index_estimate, 0] - start_time < duration:
                 index_estimate += 1
                 pass
             return index_estimate
+        pass
     else:
+        # Recursive call
         return get_next_index(data, duration, start_index, time_step,
                               index_estimate - index_change)
 
@@ -43,22 +57,22 @@ def convert_to_csv(source_name, dest_name):
     base_name = os.path.splitext(source_name)[0]
     # split a file into several trunks
     split_index = int(len(data)/split_num)
-    #for i in range(split_num):
     estimate_index = split_index
     start_index = 0
     i = 0
     while estimate_index < len(data):
         file_name = base_name + "%02d.csv" % (i + 1)
         next_index = get_next_index(data, duration, start_index, average_time_step, estimate_index)
-        #section = data[i*split_index:(i + 1)*split_index]
-        #section[:, 1:] -= data[i*split_index, 1:]
         if next_index is None:
             print("reached None")
             break
         section = data[start_index:next_index + 1]
         np.savetxt(os.path.join(dest_name, file_name), section,  delimiter=',')
+
+        # Debug print
         #print("next_index: %d, start_index: %d, estimate_index: %d" % (next_index, start_index, estimate_index))
         #print("time change: %f" % (section[-1, 0] - section[0, 0]))
+
         # Update iteration
         start_index = next_index + 1
         estimate_index = next_index + split_index
